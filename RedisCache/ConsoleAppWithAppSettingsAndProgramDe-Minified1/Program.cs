dbbdc1ba9658431e92865c322d1ae93a6f32bf2a
Brian L. Gorman <blgorman@gmail.com>
2022-05-16 10:35:54 -0500

    updates to show cross polination and priming

--- RedisCache/ConsoleAppWithAppSettingsAndProgramDe-Minified1/Program.cs
+++ RedisCache/ConsoleAppWithAppSettingsAndProgramDe-Minified1/Program.cs
@@ -64,6 +64,9 @@ namespace ConsoleAppWithAppSettingsAndProgramDe_Minified1
                         Console.WriteLine($"The next item is {item.Key} with value {value}");
                     }
                 }
+
+                //states
+                await PrimeAndUseStates(db);
             }
         }
         private static bool UtilizeTransactions(Dictionary<string, string> data, int expirationSeconds)
@@ -95,7 +98,90 @@ namespace ConsoleAppWithAppSettingsAndProgramDe_Minified1
             return transactionResult;
         }
 
-        
+        private static async Task PrimeAndUseStates(IDatabase db)
+        {
+            //determine if have cache:
+            string curCacheStates = await db.StringGetAsync("States");
+            Console.WriteLine($"GET: {curCacheStates}");
+
+            if (string.IsNullOrWhiteSpace(curCacheStates))
+            {
+                var states = GetStates();
+                string statesJSON = JsonConvert.SerializeObject(states);
+
+                Console.WriteLine("Cache response from storing States : " +
+                        await db.StringSetAsync("States", statesJSON));
+
+                curCacheStates = statesJSON;
+            }
+
+            var statesDataFromCache = JsonConvert.DeserializeObject<List<State>>(curCacheStates);
+
+            foreach (var s in statesDataFromCache)
+            {
+                Console.WriteLine($"New State {s.Id} | {s.Abbreviation} | {s.Name}");
+            }
+
+            Console.WriteLine("States data priming completed");
+        }
+
+        private static List<State> GetStates()
+        {
+            return new List<State>()
+            {
+                new State() { Id = 1, Name = "Alabama", Abbreviation = "AL" },
+                    new State() { Id = 2, Name = "Alaska", Abbreviation = "AK" },
+                    new State() { Id = 3, Name = "Arizona", Abbreviation = "AZ" },
+                    new State() { Id = 4, Name = "Arkansas", Abbreviation = "AR" },
+                    new State() { Id = 5, Name = "California", Abbreviation = "CA" },
+                    new State() { Id = 6, Name = "Colorado", Abbreviation = "CO" },
+                    new State() { Id = 7, Name = "Connecticut", Abbreviation = "CT" },
+                    new State() { Id = 8, Name = "Delaware", Abbreviation = "DE" },
+                    new State() { Id = 9, Name = "District of Columbia", Abbreviation = "DC" },
+                    new State() { Id = 10, Name = "Florida", Abbreviation = "FL" },
+                    new State() { Id = 11, Name = "Georgia", Abbreviation = "GA" },
+                    new State() { Id = 12, Name = "Hawaii", Abbreviation = "HI" },
+                    new State() { Id = 13, Name = "Idaho", Abbreviation = "ID" },
+                    new State() { Id = 14, Name = "Illinois", Abbreviation = "IL" },
+                    new State() { Id = 15, Name = "Indiana", Abbreviation = "IN" },
+                    new State() { Id = 16, Name = "Iowa", Abbreviation = "IA" },
+                    new State() { Id = 17, Name = "Kansas", Abbreviation = "KS" },
+                    new State() { Id = 18, Name = "Kentucky", Abbreviation = "KY" },
+                    new State() { Id = 19, Name = "Louisiana", Abbreviation = "LA" },
+                    new State() { Id = 20, Name = "Maine", Abbreviation = "ME" },
+                    new State() { Id = 21, Name = "Maryland", Abbreviation = "MD" },
+                    new State() { Id = 22, Name = "Massachusetts", Abbreviation = "MS" },
+                    new State() { Id = 23, Name = "Michigan", Abbreviation = "MI" },
+                    new State() { Id = 24, Name = "Minnesota", Abbreviation = "MN" },
+                    new State() { Id = 25, Name = "Mississippi", Abbreviation = "MS" },
+                    new State() { Id = 26, Name = "Missouri", Abbreviation = "MO" },
+                    new State() { Id = 27, Name = "Montana", Abbreviation = "MT" },
+                    new State() { Id = 28, Name = "Nebraska", Abbreviation = "NE" },
+                    new State() { Id = 29, Name = "Nevada", Abbreviation = "NV" },
+                    new State() { Id = 30, Name = "New Hampshire", Abbreviation = "NH" },
+                    new State() { Id = 31, Name = "New Jersey", Abbreviation = "NJ" },
+                    new State() { Id = 32, Name = "New Mexico", Abbreviation = "NM" },
+                    new State() { Id = 33, Name = "New York", Abbreviation = "NY" },
+                    new State() { Id = 34, Name = "North Carolina", Abbreviation = "NC" },
+                    new State() { Id = 35, Name = "North Dakota", Abbreviation = "ND" },
+                    new State() { Id = 36, Name = "Ohio", Abbreviation = "OH" },
+                    new State() { Id = 37, Name = "Oklahoma", Abbreviation = "OK" },
+                    new State() { Id = 38, Name = "Oregon", Abbreviation = "OR" },
+                    new State() { Id = 39, Name = "Pennsylvania", Abbreviation = "PA" },
+                    new State() { Id = 40, Name = "Rhode Island", Abbreviation = "RI" },
+                    new State() { Id = 41, Name = "South Carolina", Abbreviation = "SC" },
+                    new State() { Id = 42, Name = "South Dakota", Abbreviation = "SD" },
+                    new State() { Id = 43, Name = "Tennessee", Abbreviation = "TN" },
+                    new State() { Id = 44, Name = "Texas", Abbreviation = "TX" },
+                    new State() { Id = 45, Name = "Utah", Abbreviation = "UT" },
+                    new State() { Id = 46, Name = "Vermont", Abbreviation = "VT" },
+                    new State() { Id = 47, Name = "Virginia", Abbreviation = "VA" },
+                    new State() { Id = 48, Name = "Washington", Abbreviation = "WA" },
+                    new State() { Id = 49, Name = "West Virginia", Abbreviation = "WV" },
+                    new State() { Id = 50, Name = "Wisconsin", Abbreviation = "WI" },
+                    new State() { Id = 51, Name = "Wyoming", Abbreviation = "WY" }
+            };
+        }
 
         private static void BuildOptions()
         {
